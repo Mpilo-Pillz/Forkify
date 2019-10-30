35801aa10d03be2cbf4f7126357cefa11d5de834
Fikasentani <mpilo.dlamini@yahoo.com>
2019-10-30 06:33:45 +0200

    adding recipe view api call not getting back data

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 // import { stat } from 'fs';
 
@@ -87,7 +88,7 @@ const controlRecipe = async () => {
 
     if(id) {
         //Prepare URL for changes
-
+        renderLoader(elements.recipe);
         //Create ne recipe object
         state.recipe = new Recipe(id);
 
@@ -103,6 +104,8 @@ const controlRecipe = async () => {
        state.recipe.calcServings();
 
        //render recipe
+       clearLoader();
+       recipeView.renderRecipe(state.recipe)
        console.log(state.recipe);
         } catch (err){
             alert('Error processing recipe');
